0ca814be944d62457aabd5e18fa4c51b73523944
Facundo <facundogds@gmail.com>
2022-10-11 22:50:38 +0000

    Update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 
+//Podría agregar un event-listener y de forma
+//imperativa (DOM) generar un evento de "click"
+//sobre el botón pero esto es REACT, seré declarativo.
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<div>
-				<h1>Add a task</h1>
-				<input
-				type="text"
-				placeholder="Write a task"
-				/>
-			</div>
-		</div>
-	);
+  let task = [];
+
+  const addTaskHandler = () => {
+    altert('clickeaste');
+  };
+  
+  const inputChangeHandler = () => {}
+  
+  return (
+    <>
+      <div className="text-center">
+        <div>
+          <h1>Todo-list</h1>
+          <input onChange={null} type="text" placeholder="Write a task" />
+          <button onClick={addTaskHandler}>Add task</button>
+        </div>
+      </div>
+      <div className="text-center">
+        <ul>
+          <li>Cook</li>
+          <li>Chill</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Home;
