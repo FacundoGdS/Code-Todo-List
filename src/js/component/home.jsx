c3f76c3d1fca0f86cfaf99a1ae1285752c751cd4
Facundo <facundogds@gmail.com>
2022-10-23 05:55:28 +0000

    Fixed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 //sobre el botón pero esto es REACT, seré declarativo.
 
 const Home = () => {
-  let task = []; //Debo evitar este tipo de dato (array) ya que uso React.
+  //let task = []; //Debo evitar este tipo de dato (array) ya que uso React.
   // Debo usar un estado (hook). No es Javascript a secas. Esto es JSX.
 
   const [inputField, setInput] = useState("");//Default.
@@ -24,6 +24,10 @@ const Home = () => {
     console.log(event.target.value);
   };
 
+const deleteTask = (indexDelete) => {
+    setTasks(prevState => prevState.filter((item, index)=>index!==indexDelete));
+  };
+
   return (
     <>
       <div className="text-center">
@@ -40,7 +44,7 @@ const Home = () => {
       </div>
       <div className="text-center">
         <ul>
-        {tasks.map((task, index)=>{return <li key={index}>{task}</li>})}
+        {tasks.map((task, index)=>{return <li key={index}>{task} <button onClick={()=>deleteTask(index)}>x</button></li>})}
         </ul>
       </div>
     </>
